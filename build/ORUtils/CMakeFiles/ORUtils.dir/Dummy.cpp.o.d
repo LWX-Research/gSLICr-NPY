2a42bc4efd7b24efc5c098e351d175aa7cb4dad6
Xuan <126853724+LWX-Research@users.noreply.github.com>
2026-05-30 23:26:51 +0800

    Add files via upload

---
+++ build/ORUtils/CMakeFiles/ORUtils.dir/Dummy.cpp.o.d
@@ -0,0 +1,3 @@
+ORUtils/CMakeFiles/ORUtils.dir/Dummy.cpp.o: \
+ /root/autodl-tmp/third_party/gSLICr/ORUtils/Dummy.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/ORUtils/CMakeFiles/ORUtils.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ORUtils.dir/Dummy.cpp.o"
+  "CMakeFiles/ORUtils.dir/Dummy.cpp.o.d"
+  "libORUtils.a"
+  "libORUtils.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ORUtils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
